0c8d9a7c00d8570f5493d8b5244f7c4769de3942
Milindbhangale2002 <milind.bhangale2002@gmail.com>
2024-10-23 09:39:13 +0530

    uplaod files
    01-variable-assignment.js
    01-variableAssign.js

---
+++ 01-variable-assignment.js
@@ -0,0 +1,16 @@
+// Variable Declaration and Initialization
+var city = "Malkapur";
+console.log("City Name is: ", city);
+
+// Variable Declaration and Initialization at the same line
+var bestfriend ="Pratik";
+console.log("Bestfriend Name is :",bestfriend);
+
+var country = "Buldhana";
+console.log("Country Name is:",country);
+
+var schoolname ="Yashwant Vidyalaya ";
+console.log("School Name is:",schoolname);
+
+var collagename ="Vidnyan Mahavidyalaya";
+console.log("College Name is :",collagename);
---
+++ 01-variableAssign.js
@@ -0,0 +1,43 @@
+//Variable Declaration and Initialization
+var myFullName ="Milind Dnyandev Bhangale";
+console.log("Full Name:",myFullName)
+
+var myFullName ="Milind Bhangale";
+console.log("First And last Name:",myFullName);
+
+var myFullName ="Milind ";
+console.log("First Name:",myFullName)
+
+var myFullName ="Dnyandev";
+console.log("Middle Name:",myFullName);
+
+var myFullName ="Bhangale";
+console.log("Last Name:",myFullName)
+
+
+var myLove ="I Love Only JS";
+console.log("Language:",myLove);
+
+//Variable Declaration and Initialization at the same line
+
+var firstName ="Milind";
+console.log("First Name:", firstName)
+
+var lastName ="Bhangale";
+console.log("Last Name:",lastName);
+
+var age =22;
+console.log("Age:",age);
+
+var address ="Ulhas Nagar-4"
+console.log("Address:",address);
+
+var pinCode ="423101";
+console.log("PinCode:",pinCode);
+
+var gender = "Male";
+console.log("Gender:", gender);
+
+var isMarried = true
+console.log("isMarried:",isMarried);
+
