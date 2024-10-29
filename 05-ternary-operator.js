1da9ac2898f313cd640056b9930b7a7bd5e04ac7
Milindbhangale2002 <milind.bhangale2002@gmail.com>
2024-10-29 06:58:00 +0530

    commit files

---
+++ 05-ternary-operator.js
@@ -0,0 +1,33 @@
+function squareOFWordLength(givenstring1){
+var wordLength = givenstring1.length;
+var result = wordLength * wordLength;
+return result;
+
+}
+var returnValues =squareOFWordLength("JavaScript");
+console.log(`Given string is: Web developer smart and it's length square is:${returnValues}`);
+
+var returnValues =squareOFWordLength("Google chrome");
+console.log(`Given string is: Web developer smart and it's length square is:${returnValues}`);
+
+var returnValues =squareOFWordLength("Web developer smart");
+console.log(`Given string is: Web developer smart and it's length square is:${returnValues}`);
+
+function stringOperation(givenString) {
+    var strLength = givenString.length;
+    var words = givenString.split(" "); // Split by spaces to get words
+    var totalWords = words.length;
+
+    if (totalWords === 0) {
+        console.log("No words to operate on.");
+        return;
+    }
+
+    var strDivision = strLength / totalWords;
+    console.log(`String Division is: ${strDivision}`);
+
+    var strMultiply = strLength * totalWords;
+    console.log(`String Multiplication is: ${strMultiply}`);
+}
+
+stringOperation("I am Angular Developer");
