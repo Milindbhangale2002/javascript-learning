d794d4c6bfaa05a997ab72714eec11e9a4777e70
Milindbhangale2002 <milind.bhangale2002@gmail.com>
2024-11-06 22:59:22 +0530

    commit files

---
+++ 06-for-assignment01.js
@@ -0,0 +1,34 @@
+console.log("Print Numbers from 5 to 15 by incrementing 1");
+for (let index = 5; index <= 15; index++) {
+    console.log(index);    
+}
+
+console.log("Print Numbers from 50 to 40. decrementing by 1");
+for (let index = 50; index >= 40; index--) {
+    console.log(index);    
+}
+
+console.log("Find first 15 odd Numbers.");
+for (let index = 0; index < 30; index++) {
+    console.log(index=index+1);    
+}
+
+console.log("find first 10 even Numbers");
+for (let index = 1; index <20; index++) {
+    console.log(index=index+1);    
+}
+
+console.log("Print table of 5 ");
+for (let index = 0; index < 50; index+5) {
+    console.log(index=index+5);    
+}
+
+console.log("Print table of 10 ");
+for (let index = 0; index < 100; index+10) {
+    console.log(index=index+10);    
+}
+
+console.log("Print table of 10 in reverse order  ");
+for (let index = 100; index > 10; index-10) {
+    console.log(index=index-10);    
+}
