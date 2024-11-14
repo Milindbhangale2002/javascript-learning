a825ba7f5a8587e41804299d92d8fbd8429e7868
Milindbhangale2002 <milind.bhangale2002@gmail.com>
2024-11-14 22:31:25 +0530

    commit Files

--- 09-class.js
+++ 09-class.js
@@ -9,10 +9,28 @@ class Student{
         this.isMarried = isMarried;
     }
     // Member Function or methods
+    details(){
+        console.log(`==== Students ${this.fullName} Details ====== `);
+        console.log(`City: ${this.city}, AGE: ${this.age}, Is Married: ${this.isMarried}`);  
+    }
 }
 
 const stdElon = new Student("Elon Musk", "LA", 22, true);
-console.log(stdElon);
+stdElon.details();
+//console.log(stdElon.fullName);
+
+// console.log(stdElon);
 
 const stdStew = new Student("Stew Job", "Pune", 56, false);
-console.log(stdStew);
+// console.log(stdStew);
+stdStew.details();
+
+console.log("====== instanceof operator =======");
+
+class Department{
+
+}
+const dept = new Department();
+
+console.log(stdStew instanceof Student);
+console.log(dept instanceof Student);
